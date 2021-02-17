df38bef149b03c21ff402d5f64df9bfafec16bf2
jason <jmasters1000@yahoo.com>
2021-02-16 17:01:02 -0800

    finish nobel bonus and work on select within select

--- W5D2/SQL_Zoo/lib/04_select_within_select.rb
+++ W5D2/SQL_Zoo/lib/04_select_within_select.rb
@@ -35,13 +35,41 @@ end
 def larger_than_russia
   # List each country name where the population is larger than 'Russia'.
   execute(<<-SQL)
+  SELECT
+    name
+  FROM
+    countries
+  WHERE
+    population > (
+      SELECT
+        population
+      FROM
+        countries
+      WHERE
+        name = 'Russia'
+    )
   SQL
 end
+
 
 def richer_than_england
   # Show the countries in Europe with a per capita GDP greater than
   # 'United Kingdom'.
   execute(<<-SQL)
+  SELECT
+    name
+  FROM
+    countries
+  WHERE
+    continent = 'Europe' AND
+    gdp/population > (
+      SELECT
+        gdp/population
+      FROM
+        countries
+      WHERE
+        name = 'United Kingdom'
+    )
   SQL
 end
 
@@ -49,13 +77,35 @@ def neighbors_of_certain_b_countries
   # List the name and continent of countries in the continents containing
   # 'Belize', 'Belgium'.
   execute(<<-SQL)
+  SELECT
+    name, continent
+  FROM
+    countries
+  WHERE
+    continent IN (
+      SELECT
+        continent
+      FROM
+        countries
+      WHERE
+        name = 'Belize' OR name = 'Belgium'
+    )
   SQL
 end
+
+# Table name: countries
+#
+#  name        :string       not null, primary key
+#  continent   :string
+#  area        :integer
+#  population  :integer
+#  gdp         :integer
 
 def population_constraint
   # Which country has a population that is more than Canada but less than
   # Poland? Show the name and the population.
   execute(<<-SQL)
+  
   SQL
 end
 
