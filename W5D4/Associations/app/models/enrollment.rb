9ce9ea9d207d3a0908a9b35549cb1a7e116c959a
byoung <youngben@earthlink.net>
2021-02-18 15:35:01 -0800

    enrolled and stuff

--- W5D4/Associations/app/models/enrollment.rb
+++ W5D4/Associations/app/models/enrollment.rb
@@ -9,7 +9,7 @@
 #  updated_at :datetime         not null
 #
 class Enrollment < ApplicationRecord
-    belongs_to :student,
+    belongs_to :user,
         primary_key: :id,
         foreign_key: :student_id,
         class_name: :User
