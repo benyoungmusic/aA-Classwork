119be9ac67ca18fb0b92621124b8098a4d8a035d
christopherclow <christopher.ck.low@gmail.com>
2021-02-25 17:15:08 -0800

    almost

--- W6D3/First_Routes/db/seeds.rb
+++ W6D3/First_Routes/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Artwork.destroy_all
+User.destroy_all 
+
+user1 = User.create({username: 'Bob Ross'})
+user2 = User.create({username: 'Jim Bean'})
+user3 = User.create({username: 'Robert Downey JR'})
+
+art1 = Artwork.create([{title: 'Happy Trees'}, {image_url: 'Happy@trees.com'}, {artist_id: user1.id}])
+art2 = Artwork.create([{title: 'Whiskey'}, {image_url: 'Friday@whiskey.com'}, {artist_id: user2.id}])
+art3 = Artwork.create([{title: 'Iron Man'}, {image_url: 'RIP@trees.com'}, {artist_id: user3.id}])
+
+
+
+
