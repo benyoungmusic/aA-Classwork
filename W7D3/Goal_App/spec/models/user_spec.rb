f36a5b36fcde31add2ba66eaf060fb96c7ee22c5
Sou Lee <Sou_Lee92@yahoo.com>
2021-03-03 14:36:52 -0800

    Working on find_by_user_credentials

--- W7D3/Goal_App/spec/models/user_spec.rb
+++ W7D3/Goal_App/spec/models/user_spec.rb
@@ -8,8 +8,30 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:password_digest) }
     it { should validate_length_of(:password).is_at_least(6) }
     it { should validate_uniqueness_of(:username) }
-
   end
+
+  describe "#password?" do
+    let(:test_user) { User.create(username: 'Test', password: 'test123') }
+
+    context "valid password" do
+      it "should return true" do
+        expect(:test_user.password?(:password)).to be true
+      end
+    end
+
+    context "invalid password" do
+      it "should return false" do
+        expect(:test_user.password?('123')).to be false
+      end
+    end
+  end
+
+
+  describe "::find_by_user_credentials" do
+    let(:test_user) { User.create(username: 'Test', password: 'test123') }
+    
+  end
+
 
   
 end
