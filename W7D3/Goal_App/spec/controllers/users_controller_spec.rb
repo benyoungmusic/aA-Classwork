c07171ab169196f0f4ec5cfbdb98138b18515917
byoung <youngben@earthlink.net>
2021-03-03 16:03:15 -0800

    controller specs added

--- W7D3/Goal_App/spec/controllers/users_controller_spec.rb
+++ W7D3/Goal_App/spec/controllers/users_controller_spec.rb
@@ -9,4 +9,40 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
+    describe "POST #create" do
+        let(:user) { user.create(username: "test", password: "123123")}
+
+        context "valid params" do
+            it "should create a user" do
+                expect(user.username).to eq("test")
+            end
+
+            it "should log them in" do
+                expect(session:[:session_token]).to eq(user.session_token)
+            end
+        end
+
+        context "invalid params" do
+            it "should redirect them to sign up page" do
+                expect(response).to render_template(:new)
+            end
+
+            it "should show errors" do
+                expect(flash[:errors]).to be_present
+            end
+        end
+
+    end
+
+    describe "GET #show" do
+        let(:user) { user.create(username: "test", password: "123123")}
+
+        it "shows the user's info" do
+            get :show
+
+            expect(response).to render_template(:show)
+        end
+
+    end
+
 end
