a485575126931cdef3b1cc98743ee4ade47c2b6b
terryt925 <thindterry@yahoo.com>
2021-03-17 14:32:19 -0700

    Create grid

--- W9D3/jQuery_Tic_Tac_Toe/src/index.js
+++ W9D3/jQuery_Tic_Tac_Toe/src/index.js
@@ -2,8 +2,9 @@ const View = require("./ttt-view.js");
 const Game = require("./Tic_Tac_Toe_Node/game.js");
 
   $(() => {
-    View.prototype.setupBoard() {
-      const row = $('<ul class="ttt"></ul>')
-      
-    }
+    const game = new Game();
+    const container = $(".ttt");
+    const view = new View(game, container);
+    
+    
   });
--- W9D3/jQuery_Tic_Tac_Toe/src/ttt-view.js
+++ W9D3/jQuery_Tic_Tac_Toe/src/ttt-view.js
@@ -1,13 +1,25 @@
 class View {
   constructor(game, $el) {
-    
+    this.$el = $el
+    this.game = game
+    this.setupBoard();
   }
 
   bindEvents() {}
 
   makeMove($square) {}
 
-  setupBoard() {}
+  setupBoard() {
+    let grid = $('<ul class="grid"></ul>');
+
+    for(let i = 0; i < 9; i++) {
+      let cell = $("<li class='cell' id="+ i +" ></li>");
+      grid.append(cell);
+    }
+
+    grid.css('display', 'flex');
+    this.$el.append(grid);
+  }
 }
 
 module.exports = View;
