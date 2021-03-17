4561e1fef9bfa98213238cd5a36100b09aee63e1
byoung <youngben@earthlink.net>
2021-03-17 16:04:22 -0700

    tweaked

--- W9D3/jQuery_Tic_Tac_Toe/src/ttt-view.js
+++ W9D3/jQuery_Tic_Tac_Toe/src/ttt-view.js
@@ -13,31 +13,42 @@ class View {
     let grid = $('<ul class="grid"></ul>');
 
     for(let i = 0; i < 9; i++) {
-      let cell = $("<li class='cell' id="+ i +" data-is-clicked='false'></li>");
+      let cell = $("<li class='cell' id=" + i + "></li>");
+      cell.data('isClicked', false);
+      cell.css('border-style', 'solid');
+      cell.css('border-width', '5px');
+      cell.css('width', '190px');
+      cell.css('height', '190px');
+      cell.css('list-style', 'none');
+      cell.on('click', (e) => {
+        // debugger
+        if (!$(e.target).data('isClicked')) {
+          $(e.target).data('isClicked', true);
+        } else {
+          $(e.target).data('isClicked', false);
+        }
+      });
+  
+      cell.hover((e) => {
+        // debugger
+        if (!$(e.target).data('isClicked')) {
+          $(e.target).css('background-color', 'yellow')
+        } else {
+          $(e.target).css('background-color', 'gray')
+        }
+      }, (e) => {
+        $(e.target).css('background-color', 'gray')
+      });
       grid.append(cell);
     }
 
-    let cell = $(".cell");
-    cell.css('border-style', 'solid');
-    cell.css('border-width', '5px');
-
-    cell.on('click', () => {
-      if (!this.data('isClicked')) {
-        this.data('isClicked', true)
-      } else {
-        this.data('isClicked', false)
-      }
-    });
-
-    cell.on('hover', () => {
-      if (!this.data('isClicked')) {
-        cell.css('background-color', 'yellow')
-      }
-    });
+    // let cell = $(".cell");
 
     grid.css('display', 'flex');
     grid.css('flex-wrap', 'wrap');
     grid.css('width', '600px');
+    grid.css('height', '600px');
+
     this.$el.append(grid);
   }
 }
