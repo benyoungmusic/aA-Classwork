e1f8dcd2079adfed152aeb65720163a7f0527061
byoung <youngben@earthlink.net>
2021-03-19 13:53:59 -0700

    warmup done

--- W9D5/Pocket_Projects/src/warmup.js
+++ W9D5/Pocket_Projects/src/warmup.js
@@ -2,7 +2,9 @@
 const partyHeader = document.getElementById('party');
 
 export const htmlGenerator = (string, htmlElement) => {
-
+    const p = document.createElement("p");
+    p.innerText = string
+    htmlElement.appendChild(p)
 };
 
 htmlGenerator('Party Time.', partyHeader);
